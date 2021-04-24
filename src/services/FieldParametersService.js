ba6392f6b2ba7266f17038dd2d85b773cbe737ca
stibel <mikisiebielec@gmail.com>
2021-04-24 19:46:35 +0200

    Changed FieldParametersService to count GSD as centimeteres

--- src/services/FieldParametersService.js
+++ src/services/FieldParametersService.js
@@ -1,6 +1,6 @@
 export class FieldParametersService {
     static getFlightHeight(GSD, camera, avgHeight) {
-        const height = GSD * (camera.focalLength * Math.pow(10, -3)) / (camera.pixel * Math.pow(10, -6)) + avgHeight;
+        const height = (GSD / 100) * (camera.focalLength * Math.pow(10, -3)) / (camera.pixel * Math.pow(10, -6)) + avgHeight;
         console.log(height);
         return Math.round(height * 1000) / 1000;
     }
@@ -9,8 +9,8 @@ export class FieldParametersService {
         const dimensionX = camera.matrixDimension[1];
         const dimensionY = camera.matrixDimension[0];
 
-        const Lx = dimensionX * GSD;
-        const Ly = dimensionY * GSD;
+        const Lx = dimensionX * (GSD / 100);
+        const Ly = dimensionY * (GSD / 100);
         const Bx = Lx * (100 - p) / 100;
         const By = Ly * (100 - q) / 100;
 
